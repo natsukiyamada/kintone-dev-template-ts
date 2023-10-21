74aa7732b4f5f896aaf7746de08f6c5cf60bda0c
Natsuki <natsuki-yamada@cybozu.co.jp>
2023-10-21 15:06:41 +0900

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## 1.0.0 (2023-10-20)
+
+
+### Bug Fixes
+
+* add relase action ([9f93faa](https://github.com/natsukiyamada/explore_github/commit/9f93faa919a853d16dc3b56caf214f9a92c6ff92))
+* add relase action ([2c33841](https://github.com/natsukiyamada/explore_github/commit/2c33841f749ba39bb33d0223227836a5f797d0f0))
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: ['./src/index.ts'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: `bundle.js`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+  devtool: 'inline-source-map',
+};
